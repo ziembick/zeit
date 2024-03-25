3c2527cf5149a54f5538952842d298cf7c0fd8c5
Paulo Ziembick <pauloziembick@gmail.com>
2024-03-25 14:34:14 -0300

    promotion

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -22,6 +22,7 @@ export const dynamic = 'force-dynamic'
 
 import classes from './index.module.scss'
 import Categories from '../../_components/Categories'
+import Promotion from '../../_components/Promotion'
 
 
 export default async function Page({ params: { slug = 'home' } }) {
@@ -65,6 +66,7 @@ export default async function Page({ params: { slug = 'home' } }) {
           <Hero {...hero} />
           <Gutter className={classes.home}>
             <Categories categories={categories}/>
+            <Promotion />
           </Gutter>
         </Gutter>
       ) : (
