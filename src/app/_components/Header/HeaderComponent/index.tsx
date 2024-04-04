d77d4236f57d53cd9fdab2f7a9ac2786eba42c57
Paulo Ziembick <pauloziembick@gmail.com>
2024-04-04 13:51:46 -0300

    header responsive

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -11,8 +11,13 @@ import { HeaderNav } from '../Nav'
 import { noHeaderFooterUrls } from '../../../constants'
 import { usePathname } from 'next/navigation'
 import { NavItem } from './nav-item'
+import MobileNav from '../MobileNav'
 
 const NAV_ITEMS = [
+  {
+    label: 'SHOP ALL',
+    href: '/products',
+  },
   {
     label: 'LOOKBOOK',
     href: '/lookbook',
@@ -21,10 +26,7 @@ const NAV_ITEMS = [
     label: 'INSIDE ZEIT',
     href: '/inside',
   },
-  {
-    label: 'ONDE ENCONTRAR',
-    href: '/encontrar',
-  },
+
   {
     label: 'FALE CONOSCO',
     href: '/contato',
@@ -36,24 +38,27 @@ export default function HeaderComponent({ header }: { header: Header }) {
 
   return (
     <nav
-      className={[
-        classes.header,
-        noHeaderFooterUrls.includes(pathname) && classes.hide,
-      ]
+      className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide]
         .filter(Boolean)
         .join(' ')}
     >
       <Gutter className={classes.wrap}>
+        <MobileNav header={header} />
         <Link href="/">
-          <Image src="/logo-white.svg" alt="Logo" width={170} height={50} className={classes.imagem}/>
+          <Image
+            src="/zeit-logo.png"
+            alt="Logo"
+            width={170}
+            height={50}
+            className={classes.imagem}
+          />
         </Link>
-        <nav className={classes.teste}>
+        <nav className={classes.navItems}>
           {NAV_ITEMS.map(item => (
             <NavItem {...item} key={item.label} />
           ))}
         </nav>
         <HeaderNav header={header} />
-        {/* <MobileNav header={header} /> */}
       </Gutter>
     </nav>
   )
--- src/app/_components/Header/MobileNav/index.tsx
+++ src/app/_components/Header/MobileNav/index.tsx
@@ -1,9 +1,92 @@
-import React from 'react'
+'use client'
 
-export default function MobileNav() {
+import React, { useState } from 'react'
+import Link from 'next/link'
+import classes from './index.module.scss'
+import { HR } from '../../HR'
+import Image from 'next/image'
+
+export default function MobileNav({ header }) {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false)
+
+  function showSideBar() {
+    setIsSideBarOpen(prevState => !prevState)
+  }
+
   return (
-    <div>
-      <></>
-    </div>
+    <nav className={classes.navBar}>
+      <ul className={classes.ulBar}>
+        {/* <li className={classes.liBar}>
+          <Link href="/products" className={classes.links}>
+            SHOP
+          </Link>
+        </li>
+        <li className={classes.liBar}>
+          <Link href="/lookbook" className={classes.links}>
+            LOOKBOOK
+          </Link>
+        </li>
+        <li className={classes.liBar}>
+          <Link href="/inside" className={classes.links}>
+            INSIDE ZEIT
+          </Link>
+        </li>
+        <li className={classes.liBar}>
+          <Link href="/contact" className={classes.links}>
+            FALE CONOSCO
+          </Link>
+        </li> */}
+        <li className={classes.liBar}>
+          <Link href="#" onClick={showSideBar} className={classes.linkBar}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+              <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+            </svg>
+          </Link>
+        </li>
+      </ul>
+      <ul className={`${classes.ulBarSideBar} ${isSideBarOpen ? classes.open : ''}`}>
+        <li className={classes.liBarSideBar}>
+          <Link
+            href="#"
+            onClick={showSideBar}
+            className={`${classes.linksSideBar} ${isSideBarOpen ? classes.close : ''}`}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24">
+              <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+            </svg>
+          </Link>
+        </li>
+        <li className={classes.liBarSideBar}>
+          <Link href="/" className={classes.linksSideBar}>
+            HOME
+          </Link>
+        </li>
+        <li className={classes.liBarSideBar}>
+          <Link href="/products" className={classes.linksSideBar}>
+            SHOP ALL
+          </Link>
+        </li>
+        <li className={classes.liBarSideBar}>
+          <Link href="/lookbook" className={classes.linksSideBar}>
+            LOOKBOOK
+          </Link>
+        </li>
+        <li className={classes.liBarSideBar}>
+          <Link href="/inside" className={classes.linksSideBar}>
+            INSIDE ZEIT
+          </Link>
+        </li>
+        <li className={classes.liBarSideBar}>
+          <Link href="/contact" className={classes.linksSideBar}>
+            FALE CONOSCO
+          </Link>
+        </li>
+        <li className={classes.liBarSideBar}>
+          <Link href="https://www.instagram.com">
+            <Image src="/instagram.png" alt="Instagram" width={30} height={30} className={classes.imagens} />
+          </Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
