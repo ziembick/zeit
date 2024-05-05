1ed0776e179c9e5abe4a0b431339c78c5225be65
Paulo Ziembick <pauloziembick@gmail.com>
2024-05-05 17:43:05 -0300

    search bar ui

--- src/app/_heros/Product/index.tsx
+++ src/app/_heros/Product/index.tsx
@@ -45,7 +45,7 @@ export const ProductHero: React.FC<{
     }
     return true
   }
-
+  console.log("aquia quiaquiaquiauqi",product.title)
   return (
     <Gutter className={classes.productHero}>
       <div className={classes.mediaWrapper}>
